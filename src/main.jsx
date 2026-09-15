518a2ef17d50981006602c53d0ec0931bc28a354
P0koy <90965747+P0koy@users.noreply.github.com>
2026-09-16 01:52:09 +0400

    feat: mount FRAME99 enhancements

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-createRoot(document.getElementById('root')).render(<App />);
+import Enhancements from './Enhancements.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+    <Enhancements />
+  </>
+);
